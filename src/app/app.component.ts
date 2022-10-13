d71d5075c5c3ccf49ff254f9c6f2416fa6933276
Eddy Rubens <Eddy.Rubens@kuleuven.be>
2022-10-13 15:49:29 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MarkdownService } from 'ngx-markdown';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'staticsite';
+export class AppComponent  implements OnInit {
+  constructor(private markdownService: MarkdownService) {}
+
+  ngOnInit() {
+    console.log(`onInit`);
+    this.markdownService.renderer.heading = (text: string, level: number) => {
+      console.log(`Level: ${level} Code: ${text}`);
+      if (level === 6) {
+        return '';
+      }
+      return `<h${level}>${text}</h${level}>`;
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, SecurityContext } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import { MarkdownModule } from 'ngx-markdown';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './views/header/header.component'
+import { ContentComponent } from './views/content/content.component'
+import { AsideComponent } from './views/aside/aside.component'
+import { FooterComponent } from './views/footer/footer.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    AsideComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({
+      loader: HttpClient, // Needed to load remote md files using [src]
+      sanitize: SecurityContext.NONE, // Turn off sanitization
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
