52ed60dbe901abb8eae87662a62f6313cb827ffe
Eddy Rubens <Eddy.Rubens@kuleuven.be>
2022-10-15 00:12:24 +0200

    v0.11.0 Fix load of home.md at startup

--- src/app/models/StaticSiteDefault.ts
+++ src/app/models/StaticSiteDefault.ts
@@ -1,6 +1,6 @@
 export class StaticSiteDefault {
   private constructor() { /* noop */ } // Don't allow construction, this is a 'static' class
-  public static readonly HomeSource = '/assets/pages/home.md';
+  public static readonly HomeSource = 'assets/pages/home.md';
   public static readonly TopLevelRouteParameter = 'content';
   public static readonly ContentRoute = `${StaticSiteDefault.TopLevelRouteParameter}/:folder/:tag`;
 }
